f3748d38276614f5f6e0dc74b51cdf6eb367555c
Hayeon Baek <bhy0512@gmail.com>
2024-11-24 16:11:57 +0900

    feat: Toast 메시지를 보여주기 위해 AuthLayout에 Toaster 추가

--- src/components/layout/auth/index.tsx
+++ src/components/layout/auth/index.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom'
 
+import { Toaster } from '@ui'
+
 const AuthLayout = () => {
   return (
-    <main className='w-full min-h-screen flex flex-col justify-center items-center'>
-      <Outlet />
-    </main>
+    <>
+      <main className='w-full min-h-screen flex flex-col justify-center items-center'>
+        <Outlet />
+      </main>
+      <Toaster />
+    </>
   )
 }
 export default AuthLayout
